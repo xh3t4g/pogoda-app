93988eb8cb322c252d94dfea2c3f81320edaaa83
xh3t4g <xh3t4g@gmail.com>
2026-04-13 16:54:57 +0300

    Сделал полностью готовый htmll и css каркас

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,9 +1,49 @@
+import '../css/index.css'
+import css from '../css/HomePage.module.css'
+
 export function HomePage() {
     return <>
     
-    <div>
-        привет 
-    </div>
+            <div className={css["container"]}>
+            <form className="form">
+                <input type="text" name="inputCity" id={css["inputCity"]} placeholder="Введите город..." />
+            </form>
+
+            <span className={css["WeatherIcone"]}>
+                ⛅
+            </span>
+
+            <span className={css["WeatherTemp"]}>
+                +15
+            </span>
+
+            <div className={css["ContainerCity"]}>
+                <span className={css["TitleCity"]}>
+                    Москва
+                </span>
+
+                <span className={css["ParametrCity"]}>
+                    Переменная облочность 
+                </span>
+            </div>
+
+            <div className={css["ContainerParameters"]}>
+                <div className={css["card"]}>
+                    <h5>Ощущается</h5>
+                    <span>+11°</span>
+                </div>
+
+                <div className={css["card"]}>
+                    <h5>Влажность</h5>
+                    <span>68%</span>
+                </div>
+
+                <div className={css["card"]}>
+                    <h5>Ветер</h5>
+                    <span>5 м/с</span>
+                </div>
+            </div>
+        </div>
     
     </>
 }
