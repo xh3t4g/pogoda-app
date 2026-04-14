844c9782ecc3e9e671826663070b8d58ffff8e2a
xh3t4g <xh3t4g@gmail.com>
2026-04-14 14:53:57 +0300

    Рабочий проект, показывает погоду и меняет видео на фоне под погоду

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-createRoot(document.querySelector('#root')).render(
+createRoot(document.querySelector('#root')!).render(
     <HashRouter>
         <App />
     </HashRouter>
